b65982814723f59f8d4c4b88907194470933a96d
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2013-08-05 23:50:48 +0200

    fixed unit tests to expect correct exceptions

--- spec/unit/puppet/type/bsdportconfig_spec.rb
+++ spec/unit/puppet/type/bsdportconfig_spec.rb
@@ -22,14 +22,14 @@ describe Puppet::Type.type(:bsdportconfig) do
     it "should suport :insync as a value to :ensure" do
       Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :ensure => :insync)
     end
-    it "should raise Puppet::ResourceError when :ensure is :outofsync" do
-      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :ensure => :outofsync) }.should( raise_error(Puppet::ResourceError) )
+    it "should raise Puppet::Error when :ensure is :outofsync" do
+      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :ensure => :outofsync) }.should( raise_error(Puppet::Error) )
     end
     it "should accept empty hash as a value to :options" do
       Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :options => {})
     end
-    it "should raise Puppet::ResourceError when :options is not a hash" do
-      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :options => 1) }.should( raise_error(Puppet::ResourceError) )
+    it "should raise Puppet::Error when :options is not a hash" do
+      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :options => 1) }.should( raise_error(Puppet::Error) )
     end
     it "should accept {'A' => 'on'} as a value to :options" do
       Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :options => {'A' => 'on'})
@@ -37,16 +37,16 @@ describe Puppet::Type.type(:bsdportconfig) do
     it "should accept {'A' => 'off'} as a value to :options" do
       Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :options => {'A' => 'off'})
     end
-    it "should raise Puppet::ResourceError when :options contains invalid value" do
-      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :options => {'A' => 'foo'}) }.should( raise_error(Puppet::ResourceError) )
-      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :options => {'A' => 'once'}) }.should( raise_error(Puppet::ResourceError) )
-      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :options => {'A' => 'offset'}) }.should( raise_error(Puppet::ResourceError) )
+    it "should raise Puppet::Error when :options contains invalid value" do
+      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :options => {'A' => 'foo'}) }.should( raise_error(Puppet::Error) )
+      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :options => {'A' => 'once'}) }.should( raise_error(Puppet::Error) )
+      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :options => {'A' => 'offset'}) }.should( raise_error(Puppet::Error) )
     end
-    it "should raise Puppet::ResourceError if :portsdir is not an absolute path" do
-      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :portsdir => 'foobar') }.should( raise_error(Puppet::ResourceError) )
+    it "should raise Puppet::Error if :portsdir is not an absolute path" do
+      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :portsdir => 'foobar') }.should( raise_error(Puppet::Error) )
     end
-    it "should raise Puppet::ResourceError if :port_dbdir is not an absolute path" do
-      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :port_dbdir => 'foobar') }.should( raise_error(Puppet::ResourceError) )
+    it "should raise Puppet::Error if :port_dbdir is not an absolute path" do
+      lambda { Puppet::Type.type(:bsdportconfig).new(:name => 'www/apache22', :port_dbdir => 'foobar') }.should( raise_error(Puppet::Error) )
     end
   end
 
